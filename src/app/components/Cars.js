ac6ceb9cb9067be0717d5184ed05f5a400185925
@undrokisel <undrokisel@gmail.com>
2023-12-12 20:38:13 +0300

    added & animated why-us content

--- src/app/components/Cars.js
+++ src/app/components/Cars.js
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { Brands } from './Brands'
 import { CarsSlider } from './CarsSlider'
--- src/app/components/Why.js
+++ src/app/components/Why.js
@@ -1,12 +1,105 @@
+'use client'
+
+import Image from 'next/image'
 import React from 'react'
+
+import { MdKey, MdTrendingUp, MdHandshake } from 'react-icons/md'
+import { motion } from 'framer-motion';
+
+import { fadeIn } from '../../../variants';
 
 export const Why = () => {
   return (
     <section
-      className="section flex items-center bg-green-200"
+      className="section flex items-center"
       id="why"
     >
-      Why
+      <div className="container mx-auto">
+        <motion.h2
+          variants={fadeIn('up', 0.2)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.6 }}
+          className="h2 text-center">
+          Unmatched excellence and customer satisfaction
+        </motion.h2>
+        <motion.p
+          variants={fadeIn('up', 0.4)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.6 }}
+          className="max-w-[680px] text-center mx-auto mb-6 xl:mb-2">
+          Our dedication to providing exceptional services sets us apart from the competition.
+          From the moment you engage with us,
+          we strive to exceed your expectations in every interaction
+        </motion.p>
+        <motion.div
+          variants={fadeIn('up', 0.6)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.6 }}
+          className="hidden md:flex justify-center mb-6 xl:mb-2">
+          <Image
+            src={'/images/why/car.svg'}
+            alt=''
+            width={1060}
+            height={420}
+          />
+        </motion.div>
+        <motion.div
+          variants={fadeIn('up', 0.8)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.4}}  
+
+          className="flex flex-wrap justify-center
+          xl:grid xl:grid-cols-3 gap-4  xl:gap-y-0 xl:gap-x-[30px]"
+        >
+
+          <div
+            className="flex flex-col items-center text-center
+                      max-w-[160px] xl:max-w-none p-2 xl:p-0"
+          >
+            <MdKey className='text-accent text-[38px] mb-4' />
+            <h3 className="h3">Rent simplify and quickly</h3>
+            <p className="hidden xl:flex">
+              We prioritize your need and we go above and
+              beyond to ensure your experience with us
+              is nothing short of outstanding
+            </p>
+
+          </div>
+
+          <div
+            className="flex flex-col items-center text-center
+                      max-w-[160px] xl:max-w-none p-2 xl:p-0"
+          >
+            <MdTrendingUp className='text-accent text-[38px] mb-4' />
+            <h3 className="h3">Modern & well maintained vehicles</h3>
+            <p className="hidden xl:flex">
+              We prioritize your need and we go above and
+              beyond to ensure your experience with us
+              is nothing short of outstanding
+            </p>
+
+          </div>
+
+          <div
+            className="flex flex-col items-center text-center
+                      max-w-[160px] xl:max-w-none p-2 xl:p-0"
+          >
+            <MdHandshake className='text-accent text-[38px] mb-4' />
+            <h3 className="h3">Prompt and flexible services</h3>
+            <p className="hidden xl:flex">
+              We prioritize your need and we go above and
+              beyond to ensure your experience with us
+              is nothing short of outstanding
+            </p>
+
+          </div>
+
+        </motion.div>
+      </div>
     </section>
   )
 }
